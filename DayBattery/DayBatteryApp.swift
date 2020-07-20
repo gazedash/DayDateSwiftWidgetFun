b0b52f72ad6b6af25b498a8671c2e59369d2673e
Andrey Efremov <lodash@Andreys-MacBook-Pro.local>
2020-07-20 16:35:22 +0300

    Initial Commit

---
+++ DayBattery/DayBatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayBatteryApp.swift
+//  DayBattery
+//
+//  Created by Andrey Efremov on 20.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct DayBatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
